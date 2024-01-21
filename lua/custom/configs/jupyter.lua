b5f5bcbd1eb838a62a5a27ff93d8d8e23789935a
Florian Klein <florianbe.klein@gmail.com>
2024-01-21 09:56:41 -0500

    reformatting to separate files

---
+++ lua/custom/configs/jupyter.lua
@@ -0,0 +1,58 @@
+local jupyter_elements = {
+  {
+    "GCBallesteros/NotebookNavigator.nvim",
+    keys = {
+      { "<C-j>", function() require("notebook-navigator").move_cell "d" end },
+      { "<C-k>", function() require("notebook-navigator").move_cell "u" end },
+      { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+      { "<S-ENTER>", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+    },
+    dependencies = {
+      "echasnovski/mini.comment",
+      "hkupty/iron.nvim", -- repl provider
+      -- "akinsho/toggleterm.nvim", -- alternative repl provider
+      -- "benlubas/molten-nvim", -- alternative repl provider
+      "anuvyklack/hydra.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      local nn = require "notebook-navigator"
+      nn.setup({ activate_hydra_keys = "<leader>h" })
+    end,
+  },
+  {
+    "hkupty/iron.nvim",
+    config = function()
+      local iron = require "iron.core"
+      iron.setup({
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            python = {
+              command = { "ipython" },
+              format = require("iron.fts.common").bracketed_paste,
+            },
+          },
+          repl_open_cmd = "vertical botright 80 split",
+        },
+      })
+    end,
+  },
+  {
+  "GCBallesteros/jupytext.nvim",
+  config = true,
+  lazy = false,
+  },
+  {
+  "echasnovski/mini.hipatterns",
+    event = "VeryLazy",
+    dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
+    opts = function()
+      local nn = require "notebook-navigator"
+
+      local opts = { highlighters = { cells = nn.minihipatterns_spec } }
+      return opts
+    end,
+  },
+}
+return jupyter_elements
---
+++ lua/custom/configs/languages_and_lsps.lua
@@ -0,0 +1,64 @@
+local overrides = require("custom.configs.overrides");
+local languages_and_lsps = {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- overrde plugin configs
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+  {
+    "alaviss/nim.nvim",
+    lazy = true
+  },
+  {
+    "lervag/vimtex",
+    lazy = true
+  },
+  {
+    "neoclide/coc.nvim",
+    lazy = false,
+    branch ="release",
+    build = "yarn install --frozen-lockfile"
+  },
+  {
+    "ianks/vim-tsx",
+    lazy = true
+  },
+  {
+  "neovim/nvim-lspconfig",
+
+   dependencies = {
+     "jose-elias-alvarez/null-ls.nvim",
+     config = function()
+       require "custom.configs.null-ls"
+     end,
+   },
+   config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+   end,
+  },
+}
+
+return languages_and_lsps
